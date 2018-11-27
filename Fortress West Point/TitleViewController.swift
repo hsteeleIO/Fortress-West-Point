f9f775bbda3aba551a5ea176bda6044074900eb5
Haley <thatwouldbehaley@gmail.com>
2018-11-27 11:19:12 -0500

    Added iPad Functionality and title screen

---
+++ Fortress West Point/TitleViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TitleViewController.swift
+//  Fortress West Point
+//
+//  Created by Haley Steele on 11/27/18.
+//  Copyright © 2018 C3T Hacker. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class TitleViewController: UIViewController {
+    
+}
